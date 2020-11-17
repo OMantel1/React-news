c0b12de848872ce86b9111a4fc8caebcbed6593d
omantel <oliviermantel@gmail.com>
2020-11-17 11:59:12 +0100

    Add news filter

--- src/components/News.js
+++ src/components/News.js
@@ -6,11 +6,29 @@ class News extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            articles : []
+            articles : [],
+            filtered : false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event){
+        let clickedFilterId = event.target.id;
+        if (clickedFilterId !== "all") {
+            this.setState(state =>({
+                filtered: true,
+                filter: clickedFilterId,
+            }))
         }
+
+        if (clickedFilterId === "all") {
+            this.setState(state =>({
+                filtered: false,
+            }))
+        }        
     }
     componentDidMount() {
-        fetch('http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-16&sortBy=publishedAt&apiKey=156ffdbdcc3b49fe83f4e1e4d12c9c85')
+        fetch('http://newsapi.org/v2/top-headlines?country=fr&apiKey=156ffdbdcc3b49fe83f4e1e4d12c9c85')
         .then(response => response.json())
         .then(result => {
             console.log(result.articles);
@@ -20,19 +38,37 @@ class News extends React.Component {
         })
     }
     render(){
-        const articles = this.state.articles;
+        let articlesList;
+        let articleSourceList = this.state.articles;
+
+        if(this.state.filtered){
+            articlesList = this.state.articles.filter( article => article.source.name === this.state.filter)
+        }else {
+            articlesList = this.state.articles
+        }
+
         return(
-            <div className="article-list">
-               {articles.map(article =>(
-                <ArticleResume 
-                title={article.title}
-                image={article.urlToImage}
-                description={article.description}
-                date={article.publishedAt}
-                source={article.source.name}
-                />
-               ))}
+            <div>
+                <div className="filter">
+                    <p className="filter__button"  id="all" onClick={this.handleClick}>All</p>
+                    {articleSourceList.map(article => (
+                    <p  className="filter__button" key={article.publishedAt + "1"}  onClick={this.handleClick} id={article.source.name}>{article.source.name}</p>
+                    ))} 
+                </div>
+
+                <div className="article-list">
+                {articlesList.map(article =>(
+                    <ArticleResume 
+                    title={article.title}
+                    image={article.urlToImage}
+                    description={article.description}
+                    date={article.publishedAt}
+                    source={article.source.name}
+                    />
+                    ))}  
+                </div>
             </div>
+            
         )
     }
 }
