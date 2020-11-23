2e5ac2e8358b1d81811eac3a8a8991568c22b607
omantel <oliviermantel@gmail.com>
2020-11-23 23:25:50 +0100

    refactor index

--- src/index.js
+++ src/index.js
@@ -7,20 +7,16 @@ import "./index.css";
 import Header from "./components/Header";
 import News from "./components/News";
 import Article from "./components/Article";
-import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+      <Router>
         <Header />
         <Switch>
           <Route path="/" exact component={News}></Route>
-          <Route path="/news" exact component={Article}></Route>
+          <Route path="/news/:param" component={Article}></Route>
         </Switch>
-        <Footer/>
-      </div>
-    </Router>
+      </Router>
   );
 }
 
