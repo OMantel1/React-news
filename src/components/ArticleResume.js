9fb8f3f24621ce1699f2fba1953fc067467f860c
omantel <oliviermantel@gmail.com>
2020-11-23 23:28:30 +0100

    Update ArticleResume classNames

--- src/components/ArticleResume.js
+++ src/components/ArticleResume.js
@@ -3,19 +3,20 @@ import {Link} from 'react-router-dom'
 class ArticleResume extends React.Component {
 
     render(){
+        let infos = this.props.source + "_"+ this.props.date
         return(
-            <div className= "article"> 
-                <img src={this.props.image || "./img-news.png"} className="article__img  border" alt={this.props.title}></img>
-                <h2 className=" article__title  border">
-                    <Link to={`/news`} > {this.props.title.length > 90? this.props.title.substring(0, 90) + '...':this.props.title}</Link>
+            <div className= "resume"> 
+                <img src={this.props.image || "./img-news.png"} className="resume__img  border" alt={this.props.title}></img>
+                <h2 className=" resume__title  border">
+                    <Link to={`/news/:${infos}`} > {this.props.title.length > 90? this.props.title.substring(0, 90) + '...':this.props.title}</Link>
                 </h2>
 
-                <p className=" article__content  border">{this.props.description}</p>
-                <p className=" article__source  border"><i>{this.props.source}</i></p>
+                <p className=" resume__content  border">{this.props.description}</p>
+                <p className=" resume__source  border"><i>{this.props.source}</i></p>
 
             </div>
         )
     }
-}
+} 
 
 export default ArticleResume;
