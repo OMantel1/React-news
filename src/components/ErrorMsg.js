a46b7b0d82cbf2c1c7c4df31fbc0e0990bdbe28f
omantel <oliviermantel@gmail.com>
2020-12-07 16:04:44 +0100

    Add msg components

---
+++ src/components/ErrorMsg.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ErrorMsg extends React.Component {
+  render() {
+    return (
+      <div className="error">
+        Sorry! we can't find the page you looking for.
+      </div>
+    );
+  }
+}
+
+export default ErrorMsg;
